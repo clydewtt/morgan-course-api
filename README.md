ec18e0e3b92c41754ccffe53a4f36defb2fb3b0a
ClydeWTT <tandjong.c@gmail.com>
2024-11-26 10:39:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Morgan Course Data
+
+A Python package for querying and using Morgan State University course data.
+
+## Installation
+
+```bash
+pip install morgan_course_data
+```
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="morgan_course_data",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["pymongo"],
+    description="Python package for querying Morgan State University course data.",
+    author="Clyde Tandjong",
+    author_email="cltandjong@gmail.com",
+    url="https://github.com/yourgithub/morgan_course_data",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
