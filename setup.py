c3a8a09b59af4c89503af164e8db93815b799d50
ClydeWTT <tandjong.c@gmail.com>
2024-11-26 10:40:43 -0500

    update setup url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Python package for querying Morgan State University course data.",
     author="Clyde Tandjong",
     author_email="cltandjong@gmail.com",
-    url="https://github.com/yourgithub/morgan_course_data",
+    url="https://github.com/clydewtt/morgan-course-api",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
