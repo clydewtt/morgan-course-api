c789e16b98fb9601ac0126d522f58d8cafd3bcc7
ClydeWTT <tandjong.c@gmail.com>
2024-11-29 17:11:32 -0500

    migrate

--- morgan_course_data/db.py
+++ morgan_course_data/db.py
@@ -1,8 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://read:xtUxRpHUrVMhNI8T@courses.oqzo0.mongodb.net/?retryWrites=true&w=majority&appName=courses"
-
+uri = "mongodb+srv://read:0kS4fcyXXOqWwSpt@coursedatacluster.r4jik.mongodb.net/?retryWrites=true&w=majority&appName=CourseDataCluster"
 
 def get_db_connection(db_name):
     """
